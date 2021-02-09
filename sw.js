bd5930069fe32ab08cb957548920a9920e9032c7
chibing.fy <chibing.fy@alibaba-inc.com>
2021-02-09 16:57:25 +0800

    Updates

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(e) {
-  self.registration.unregister()
-    .then(function() {
-      return self.clients.matchAll();
-    })
-    .then(function(clients) {
-      clients.forEach(client => client.navigate(client.url))
-    });
-});
+self.addEventListener("install",function(n){self.skipWaiting()}),self.addEventListener("activate",function(n){self.registration.unregister().then(function(){return self.clients.matchAll()}).then(function(n){n.forEach(n=>n.navigate(n.url))})});
